dc80bb5eb483265ea3a030475c4713a39ffe925b
Asaduzzaman Sohun <sohan138ca@gmail.com>
2022-12-26 00:39:31 +0600

    app commit

---
+++ app/src/main/java/com/sohun/blackhole/MainActivity.java
@@ -0,0 +1,24 @@
+package com.sohun.blackhole;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
